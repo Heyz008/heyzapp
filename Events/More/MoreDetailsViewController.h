8a8f508ba9a21c630d7d6a8a15c150ef8547565f
Joshua Jiang <ShuaJJ@Jiaqis-iMac.local>
2015-01-18 23:03:38 -0500

    My Initial commit

---
+++ Events/More/MoreDetailsViewController.h
@@ -0,0 +1,12 @@
+//
+//  MoreDetailsViewController.h
+//  Events
+//
+//  Copyright (c) 2014 Teknowledge Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoreDetailsViewController : UIViewController
+
+@end
---
+++ Events/ProgramViewController/ProgramViewController.m
@@ -0,0 +1,204 @@
+//
+//  ProgramViewController.m
+//  Events
+//
+//  Created by Shabbir Hasan Zaheb on 22/02/14.
+//  Copyright (c) 2014 Teknowledge Software. All rights reserved.
+//
+
+#import "ProgramViewController.h"
+#import "ProgramCustomCell.h"
+#import "EventList.h"
+#import "MyEventTickets.h"
+#import "AboutViewController.h"
+#import "UIImageView+WebCache.h"
+
+@interface ProgramViewController (){
+    
+    NSMutableArray *arrayEventList;
+}
+
+@end
+
+@implementation ProgramViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+#pragma mark - View Life Cycle
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+
+-(void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:YES];
+    
+    if ([self respondsToSelector:@selector(edgesForExtendedLayout)]) {
+        self.edgesForExtendedLayout = UIRectEdgeNone;
+    }
+
+    [Utility afterDelay:0.01 withCompletion:^{
+        [DSBezelActivityView newActivityViewForView:self.view.window];
+        [self getEventListFromServer];
+    }];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/**
+ *  getEventListFromServer
+ *
+ *  @return This method will get events data from server and store all data in EventList modal class.
+ */
+
+#pragma mark - Get All Event's from server
+-(void)getEventListFromServer
+{
+    NSDictionary *dictOfEventRequestParameter = [[NSDictionary alloc] initWithObjectsAndKeys:[Utility getNSUserDefaultValue:KUSERID],@"user_id", nil];
+
+    [Utility GetDataForMethod:NSLocalizedString(@"GETEVENTS_METHOD", @"GETEVENTS_METHOD") parameters:dictOfEventRequestParameter key:@"" withCompletion:^(id response){
+        
+        [DSBezelActivityView removeViewAnimated:NO];
+        arrayEventList  =   [[NSMutableArray alloc] init];
+        
+        if ([response isKindOfClass:[NSArray class]]) {
+            if ([[[response objectAtIndex:0] allKeys] containsObject:@"status"]) {
+                if ([[[response objectAtIndex:0] objectForKey:@"status"] intValue] == 0) {
+                    [Utility alertNotice:@"" withMSG:[[response objectAtIndex:0] objectForKey:@"message"] cancleButtonTitle:@"OK" otherButtonTitle:nil];
+                    gArrayEvents = [[NSMutableArray alloc] initWithArray:arrayEventList];
+                    [self.tableView reloadData];
+                    return ;
+                    
+                }
+            }
+            
+            NSSortDescriptor *descriptor=[[NSSortDescriptor alloc] initWithKey:@"event_start_date"  ascending:YES selector:@selector(localizedCaseInsensitiveCompare:)];
+            NSArray *sortedArrayEventList = [response sortedArrayUsingDescriptors:@[descriptor]];
+            
+            for (NSDictionary *dict in sortedArrayEventList) {
+                EventList *eventObj = [[EventList alloc] init];
+                eventObj.eventID                =   [NSNumber numberWithInt:[[dict objectForKey:@"event_id"] intValue]];
+                eventObj.eventName              =   [dict objectForKey:@"event_name"];
+                eventObj.eventImageURL          =   [dict objectForKey:@"event_image_url"];
+                eventObj.eventDescription       =   [dict objectForKey:@"event_content"];
+                
+                //12.15pm 4 June '14
+                eventObj.eventStartDateTime     =   [Utility getFormatedDateString:[NSString stringWithFormat:@"%@ %@",[dict objectForKey:@"event_start_date"],[dict objectForKey:@"event_start_time"]] dateFormatString:@"yyyy-MM-dd HH:mm:ss" dateFormatterString:@"HH:mm a - dd MMM yyyy"];
+                
+                eventObj.eventEndDateTime       =   [Utility getFormatedDateString:[NSString stringWithFormat:@"%@ %@",[dict objectForKey:@"event_end_date"],[dict objectForKey:@"event_end_time"]] dateFormatString:@"yyyy-MM-dd HH:mm:ss" dateFormatterString:@"HH:mm a - dd MMM yyyy"];
+                
+                eventObj.eventLocationName      =   [dict objectForKey:@"location_name"];
+                eventObj.eventLocationAddress   =   [dict objectForKey:@"location_address"];
+                eventObj.eventLocationTown      =   [dict objectForKey:@"location_town"];
+                eventObj.eventLocationState     =   [dict objectForKey:@"location_state"];
+                eventObj.eventLocationCountry   =   [dict objectForKey:@"location_country"];
+                eventObj.eventLocationLatitude  =   [NSNumber numberWithFloat:[[dict objectForKey:@"location_latitude"] floatValue]];
+                eventObj.eventLocationLongitude =   [NSNumber numberWithFloat:[[dict objectForKey:@"location_longitude"] floatValue]];
+                
+                if ([[dict objectForKey:@"ticket"] count]>0) {
+                    NSDictionary *dictOfTicket  =   [[NSDictionary alloc] initWithDictionary:[dict objectForKey:@"ticket"]];
+                    eventObj.eventTicketName            =   [dictOfTicket objectForKey:@"ticket_name"];
+                    eventObj.eventTicketDescription     =   [dictOfTicket objectForKey:@"ticket_description"];
+                    eventObj.eventTicketPrice           =   [dictOfTicket objectForKey:@"ticket_price"];
+                    eventObj.eventTicketStart           =   [dictOfTicket objectForKey:@"ticket_start"];
+                    eventObj.eventTicketEnd             =   [dictOfTicket objectForKey:@"ticket_end"];
+                    eventObj.eventTicketMembers         =   [dictOfTicket objectForKey:@"ticket_members_roles"];
+                    eventObj.eventTicketGuests          =   [dictOfTicket objectForKey:@"ticket_guests"];
+                    eventObj.eventTicketRequired        =   [dictOfTicket objectForKey:@"ticket_required"];
+                    eventObj.eventTicketAvailSpaces     =   [NSNumber numberWithInt:[[dictOfTicket objectForKey:@"avail_spaces"] intValue]];
+                    eventObj.eventTicketBookedSpaces    =   [NSNumber numberWithInt:[[dictOfTicket objectForKey:@"booked_spaces"] intValue]];
+                    eventObj.eventTicketTotalSpaces     =   [NSNumber numberWithInt:[[dictOfTicket objectForKey:@"total_spaces"] intValue]];
+                }
+                
+                [arrayEventList addObject:eventObj];
+            }
+        }
+        
+        /**
+         *  global array for events data.
+         */
+        gArrayEvents = [[NSMutableArray alloc] initWithArray:arrayEventList];
+        
+        [self.tableView reloadData];
+        
+    }WithFailure:^(NSString *error)
+     {
+         [DSBezelActivityView removeViewAnimated:NO];
+         NSLog(@"%@",error);
+     }];
+}
+
+#pragma mark - Table view data source
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+     return [arrayEventList count];
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 162;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"ProgramCustomCell";
+    ProgramCustomCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    if(!cell)
+    {
+        cell = [[ProgramCustomCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
+    }
+    EventList *obj = [arrayEventList objectAtIndex:indexPath.row];
+
+    cell.lblDateTime.text   =   [Utility compareDates:obj.eventStartDateTime date:[NSDate date]];
+    cell.lblEventName.text  =   obj.eventName;
+    cell.lblEventDesc.text  =   [obj.eventDescription stringByConvertingHTMLToPlainText];
+    cell.lblEventDesc.text  =   [Utility TrimString:cell.lblEventDesc.text];
+    
+    if ([obj.eventImageURL length]>0) {
+        [cell.imgEventImage setImageWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"%@",obj.eventImageURL]] placeholderImage:nil];
+        [cell.imgEventImage setContentMode:UIViewContentModeScaleAspectFit];
+        [cell.imgEventImage setClipsToBounds:YES];
+    }
+    
+    cell.imgEventImage.contentMode = UIViewContentModeScaleAspectFill;
+    
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    
+    return cell;
+}
+
+#pragma mark - Navigation
+// In a story board-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+    
+    NSIndexPath *selectedRowIndex = [self.tableView indexPathForSelectedRow];
+    AboutViewController *aboutVwController = [segue destinationViewController];
+    EventList *obj  =   [arrayEventList objectAtIndex:selectedRowIndex.row];
+    
+    aboutVwController.eventObj  =   obj;
+}
+
+@end
