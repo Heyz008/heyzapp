7ad17d6adba12766eab6b3854ee61f83b3c2a0ea
Jay Yu <jay.yzhe@gmail.com>
2015-01-27 15:49:44 -0500

    added chat, and firebase initial setup

---
+++ Events/Chat/ChatViewController.swift
@@ -0,0 +1,90 @@
+//
+//  ChatViewController.swift
+//  Chat
+//
+//  Created by My App Templates Team on 24/08/14.
+//  Copyright (c) 2014 My App Templates. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    @IBOutlet var viewForTitle : UIView!
+    @IBOutlet var ctrlForChat : UISegmentedControl!
+    @IBOutlet var btnForLogo : UIButton!
+    @IBOutlet var itemForSearch : UIBarButtonItem!
+    @IBOutlet var tblForChat : UITableView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        // Custom initialization
+    }
+    
+    required init(coder aDecoder: NSCoder)
+    {
+        super.init(coder: aDecoder)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.titleView = viewForTitle
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: btnForLogo)
+        self.navigationItem.rightBarButtonItem = itemForSearch
+        self.tabBarController?.tabBar.tintColor = UIColor.greenColor()
+        // self.performSegueWithIdentifier("loginSegue", sender: nil)
+        
+       // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func unwindToChat (segueSelected : UIStoryboardSegue) {
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView!) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        switch segmentedControl.selectedSegmentIndex{
+        case 0:
+            return tblForChat.dequeueReusableCellWithIdentifier("ChatPublicCell") as UITableViewCell
+        default:
+            return tblForChat.dequeueReusableCellWithIdentifier("ChatPrivateCell") as UITableViewCell
+            
+        }
+        
+    }
+    
+    func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!){
+        self.performSegueWithIdentifier("slideToChat", sender: nil);
+    }
+
+    @IBAction func indexChanged(sender: UISegmentedControl) {
+        self.tableView.reloadData()
+    }
+    
+    
+    /*
+    // #pragma mark - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Events/Heyz-Swift-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  SwiftOC-Bridging-Header.h
+//  Heyz
+//
+//  Created by Jay Yu on 2015-01-26.
+//  Copyright (c) 2015 Teknowledge Software. All rights reserved.
+//
+
+#ifndef Heyz_SwiftOC_Bridging_Header_h
+#define Heyz_SwiftOC_Bridging_Header_h
+
+#endif
+
+#import <Firebase/Firebase.h>
