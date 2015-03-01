f112cf95d7aa47ad95d4bc27e080fb7b4241e6f3
Joshua <Sody_007@yahoo.ca>
2015-03-01 00:08:23 -0500

    added location search page

---
+++ Events/ProgramViewController/EventLocationViewController.m
@@ -0,0 +1,13 @@
+//
+//  EventLocationViewController.m
+//  Heyz
+//
+//  Created by Joshua Jiang on 2015-02-27.
+//  Copyright (c) 2015 Teknowledge Software. All rights reserved.
+//
+
+#import "EventLocationViewController.h"
+
+@implementation EventLocationViewController
+
+@end
---
+++ Events/ProgramViewController/ProgramCustomCell/LocationCell.h
@@ -0,0 +1,13 @@
+//
+//  LocationCell.h
+//  Heyz
+//
+//  Created by Joshua Jiang on 2015-02-27.
+//  Copyright (c) 2015 Teknowledge Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocationCell : UITableViewCell
+
+@end
