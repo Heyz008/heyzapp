355578b7542463f112d75ea888df926cc218500b
Cecil Yang <yangzhichaocc@gmail.com>
2015-04-02 18:44:16 -0400

    changed icons.

---
+++ Events/TabBarController/CYTabBarController.swift
@@ -0,0 +1,36 @@
+//
+//  CYTabBarController.swift
+//  Heyz
+//
+//  Created by Zhichao Yang on 2015-04-02.
+//  Copyright (c) 2015 Teknowledge Software. All rights reserved.
+//
+
+import UIKit
+
+class CYTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
